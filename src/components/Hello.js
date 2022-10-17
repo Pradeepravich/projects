3ce6cdcfc397f45c75295198893a130b15ad1393
pradeepravishankar <pradeepravishankar.chinthala@gmail.com>
2022-10-17 17:36:49 +0530

    first commit

---
+++ src/components/Hello.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hello = () => {
+  return (
+    <div>
+        <center>
+            <h1>Welcome to Hello World. </h1>
+        </center>
+    </div>
+  )
+}
+
+export default Hello
